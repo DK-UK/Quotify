e6dd3d79ff4795fb0f29938395e402a558229675
DK <dk563177@gmail.com>
2021-10-09 15:54:48 +0530

    quote added

--- app/src/main/java/com/enjay/quotify/MainActivity.kt
+++ app/src/main/java/com/enjay/quotify/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.enjay.quotify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.enjay.quotify.ViewModel.MainViewModel
+import com.enjay.quotify.ViewModel.ViewModelFactory
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var txtQuoteText : TextView
-    private lateinit var txtQuoteAuthor : TextView
-    private lateinit var floatingShare : FloatingActionButton
+    private val txtQuoteText : TextView
+        get() = findViewById(R.id.txt_quote_text)
+
+    private val txtQuoteAuthor : TextView
+        get() = findViewById(R.id.txt_quote_author)
+
+    private val floatingShare : FloatingActionButton
+        get() = findViewById(R.id.floting_btn_share)
+
+    private lateinit var mainViewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainViewModel = ViewModelProvider(this,ViewModelFactory(application)).get(MainViewModel::class.java)
+        setQuote(mainViewModel.getQuote())
+        Log.e("Dhaval", "onCreate: QUOTE : " + mainViewModel.getQuote())
+    }
 
+    fun setQuote(quote: Quote){
+        txtQuoteText.text = quote.quoteText
+        txtQuoteAuthor.text = quote.quoteAuthor
+    }
+
+    fun onNext(view: View) {
+        setQuote(mainViewModel.getNextQuote())
+    }
+
+    fun onPrevious(view: View) {
+        setQuote(mainViewModel.getPreviousQuote())
+    }
+
+    fun onShare(view: View) {
+        var shareQuoteIntent = Intent(Intent.ACTION_SEND)
+        shareQuoteIntent.setType("text/plain")
+        shareQuoteIntent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().quoteText)
+        startActivity(shareQuoteIntent)
     }
 }
--- app/src/main/java/com/enjay/quotify/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/enjay/quotify/ViewModel/MainViewModel.kt
@@ -1,17 +1,37 @@
 package com.enjay.quotify.ViewModel
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.enjay.quotify.Quote
+import com.google.gson.Gson
+import java.nio.charset.Charset
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val context: Context) : ViewModel() {
 
-    private lateinit var quoteList : Array<Quote>
+    private var quoteList : Array<Quote> = emptyArray()
     private var index : Int = 0
+
+    init {
+        quoteList = getQuotesFromAssets(context)
+
+        Log.e("Dhaval", "QUOTE LIST : ${quoteList[0]}")
+    }
+
 
     fun getQuote() : Quote = quoteList[index]
 
     fun getNextQuote() : Quote = quoteList[++index]
     fun getPreviousQuote() : Quote = quoteList[--index]
 
-
+    fun getQuotesFromAssets(context: Context) : Array<Quote> {
+        val inputStream = context.assets.open("quotes.json")
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer,Charsets.UTF_8)
+        val gson = Gson()
+        return gson.fromJson(json,Array<Quote>::class.java)
+    }
 }
